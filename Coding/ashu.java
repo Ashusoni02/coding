74058a34f2540226c94e91ff15577e377017b328
Ansh7473 <your-github-email@example.com>
2026-05-03 14:16:46 +0530

    Add initial HTML and CSS files for Netflix clone project
    - Created netflix.html with basic structure including navigation and logo.
    - Added style.css for layout and styling, including background image and navigation styles.

--- Coding/ashu.java
+++
@@ -1,7 +0,0 @@
-package Coding;
-
-public class ashu {
-    public static void main(String[] args) {
-        System.out.println("Hello World");
-    }
-}
---
+++ Coding/findmin.java
@@ -0,0 +1,18 @@
+package Coding;
+
+public class findmin {
+    public static void main(String[] args) {
+        int[] arr = {5, 6, 7, 8, 9, 1, 2, 3, 4};
+        int result = minimum(arr);
+        System.out.println("The minimum element in the rotated sorted array is: " + result);
+    }
+    static int minimum(int[] arr) {
+        int min = arr[0];
+        for (int i = 1; i < arr.length; i++) {
+            if (arr[i] < min) {
+                min = arr[i];
+            }
+        }
+        return min;
+    }
+}
