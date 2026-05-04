1bc13a8e61df8e4cb6b2a39ae11b18398bd4112f
Ashusoni02 <soniashu022@gmail.com>
2026-05-04 22:06:25 +0530

    question

---
+++ Coding/evendigit.java
@@ -0,0 +1,45 @@
+package Coding;
+
+// https://leetcode.com/problems/find-numbers-with-even-number-of-digits/
+public class evendigit {
+
+    public static void main(String[] args) {
+        int nums[] = { 12, 345, 2, 6, 7896 };
+        System.out.println(findNumbers(nums));
+    }
+
+    // number of numbers that has even numbers of digits.
+    static int findNumbers(int[] nums) {
+
+        int count = 0;
+        for (int num : nums) {
+            if (even(num)) {
+                count++;
+            }
+
+        }
+
+        return count;
+    }
+
+    // it gives us the digit of no. is even of not.
+    static boolean even(int num) {
+        int numofdigit = digit(num);
+        if (numofdigit % 2 == 0) {
+            return true;
+        }
+        return false;
+
+    }
+
+    // it shows how many digits are in number.
+    static int digit(int num) {
+        int count = 0;
+        while (num > 0) {
+            count++;
+            num = num / 10;
+
+        }
+        return count;
+    }
+}
