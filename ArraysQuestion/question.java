cafbf3ac53c9c13ec807c7de56e30b7621be8332
Ashusoni02 <soniashu022@gmail.com>
2026-05-10 20:16:06 +0530

    eeegh

--- ArraysQuestion/question.java
+++ ArraysQuestion/question.java
@@ -1,10 +1,33 @@
 package ArraysQuestion;
 
 public class question {
-    public static void main(String[] args) {
-        
+  public static void main(String[] args) {
+    int[] nums = { 1, 2, 2, 2, 2, 4, 5 };
+    int target = 2;
+    System.out.println(occurance(nums, target));
+  }
+
+  static int occurance(int[] nums, int target) {
+    int count = 0;
+    int start = 0;
+    int end = nums.length - 1;
+
+    for (int i = 0; i < nums.length; i++) {
+      for (int j = nums.length; j > nums[0]; j--) {
+        if (target == nums[i]) {
+          start = i;
+        }
+        if (target == nums[j]) {
+          end = j;
+        }else{
+          
+        }
+
+        return count = end - start;
+      }
+
     }
-      public static void rotate(int[] nums, int k) {
-        
-    }
+    return -1;
+
+  }
 }
