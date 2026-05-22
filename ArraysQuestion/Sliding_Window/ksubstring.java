05ceddd89d180d205e57c7fcb43a4113c1588951
Ashusoni02 <soniashu022@gmail.com>
2026-05-22 21:38:29 +0530

    x

---
+++ ArraysQuestion/Sliding_Window/ksubstring.java
@@ -0,0 +1,17 @@
+package ArraysQuestion.Sliding_Window;
+
+import java.util.HashMap;
+
+public class ksubstring {
+    public static void main(String[] args) {
+       String[] st ={ "a","a","b","b","c"};  
+    }
+   static int  longestKSubstr(String s, int k) {
+        // code here
+        int low = 0;
+        int high = 0;
+        int res = Integer.MAX_VALUE;
+        HashMap<String ,Integer> map = new HashMap<>();
+     String n = st.length();
+    }
+}
