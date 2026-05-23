7619aeaaf59e8dead1006892e64b6f22f507117b
Ashusoni02 <soniashu022@gmail.com>
2026-05-23 13:48:41 +0530

    g

---
+++ javascript/script.js
@@ -0,0 +1,9 @@
+alert("ivvj");
+var a  = prompt("enter your number");
+var True  = confirm("are you sure to login");
+if(True){
+  console.log(  "login succesful");
+}else{
+     console.log("not successsful"); 
+}
+console.log(" your number is" +  a);
