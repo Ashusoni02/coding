5c416c31df71463a1c8f884b0c533b12da47c54a
Ashusoni02 <soniashu022@gmail.com>
2026-05-23 13:11:42 +0530

    dij

--- ArraysQuestion/Sliding_Window/ksubstring.java
+++ ArraysQuestion/Sliding_Window/ksubstring.java
@@ -1,17 +1,36 @@
 package ArraysQuestion.Sliding_Window;
 
 import java.util.HashMap;
+import java.util.*;
 
 public class ksubstring {
-    public static void main(String[] args) {
-       String[] st ={ "a","a","b","b","c"};  
-    }
-   static int  longestKSubstr(String s, int k) {
-        // code here
-        int low = 0;
-        int high = 0;
-        int res = Integer.MAX_VALUE;
-        HashMap<String ,Integer> map = new HashMap<>();
-     String n = st.length();
-    }
+   public static void main(String[] args) {
+      String st = "aabacbebebe";
+      System.out.println(longestKSubstr(st, 3));
+   }
+
+   static int longestKSubstr(String st, int k) {
+      // code here
+      int low = 0;
+      int res = -1;
+      int n = st.length();
+      HashMap<Character, Integer> map = new HashMap<>();
+      for (int high = 0; high < n; high++) {
+         char c = st.charAt(high);
+         map.put(c, map.getOrDefault(c, 0) + 1);
+         while (map.size() > k) {
+            char leftchar = st.charAt(low);
+            map.put(leftchar, map.get(leftchar) - 1);
+            if (map.get(leftchar) == 0) {
+               map.remove(leftchar);
+            }
+            low++;
+         }
+         if (map.size() == k) {
+            res = Math.max(res, high - low + 1);
+         }
+      }
+      return res;
+   }
+
 }
