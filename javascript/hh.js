26a2502e46c5922e385b9bea7bce16903432b40d
Ashu <soniashu022@gmail.com>
2026-05-24 19:18:10 +0530

    Add files via upload

---
+++ javascript/hh.js
@@ -0,0 +1,7 @@
+  console.log(  "login succesful");
+  console.log(  "login succesful");
+//   console.log(  "login succesful");
+  let a = 4;
+  let b = 5;
+  let    c = a + b;
+  console.log("the sum of a and b is " + c);
