a1611c55aed0e48bc9a8b2a15f0ce02297de996e
Ashu <soniashu022@gmail.com>
2026-05-28 17:48:17 +0530

    Add files via upload

---
+++ practic_Questions/c.java
@@ -0,0 +1,13 @@
+package practic_Questions;
+import java.util.Scanner;
+
+public class c {
+    public static void main(String[] args) {
+          Scanner scn = new Scanner(System.in);
+          System.out.print("enter your no.: ");
+        int n = scn.nextInt();
+        for (int i = 1; i*i <=n; i++) {
+  System.out.print(i*i +" ");
+        }
+    }
+}
---
+++ practic_Questions/sol.java
@@ -0,0 +1,24 @@
+package practic_Questions;
+import java.util.*;
+
+public class sol{
+    public static void main(String[] args){
+   
+
+        Scanner scn = new Scanner(System.in);
+        int n = scn.nextInt();
+     int inr =0;
+     int op =1;
+     while(n!=0){
+        int od = n%10;
+        int id = op;
+        int ip = od;
+
+
+         inr = inr + id * (int)Math.pow(10,ip-1);
+         n= n/10;
+         op++; 
+     }
+  System.out.println(inr);
+    }
+}
