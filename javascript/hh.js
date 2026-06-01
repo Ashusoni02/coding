412f196697c254191d2b216a83829f91839721d1
Ashu <soniashu022@gmail.com>
2026-06-01 21:15:45 +0530

    Add files via upload

--- javascript/hh.js
+++ javascript/hh.js
@@ -1,21 +1,18 @@
-
- let random= Math.random()
- console.log(random)
- let a = prompt("enter your first number: ")
- let c = prompt("enter your operator : ")
- let b = prompt("enter your se cond number: ")
- let obj = {
-     "+" : "-",
-     "*" : "+",
-     "/" : "**",
-     "-" : "/"
-    }
-     if(random>0.1){
-        console.log("the result is :" +a+c+b)
-        alert("the result is : " + eval(a+c+b))
-    }else{
-     
-        console.log("the result is :" +a+c+b)
-        alert(`the result is ${eval(`${a}${obj[c]}${b}`)}`)
-        
-     }
+let random = Math.random();
+console.log(random);
+let a = prompt("enter your first number: ");
+let c = prompt("enter your operator : ");
+let b = prompt("enter your se cond number: ");
+let obj = {
+  "+": "-",
+  "*": "+",
+  "/": "**",
+  "-": "/",
+};
+if (random > 0.1) {
+  console.log("the result is :" + a + c + b);
+  alert("the result is : " + eval(a + c + b));
+} else {
+  console.log("the result is :" + a + c + b);
+  alert(`the result is ${eval(`${a}${obj[c]}${b}`)}`);
+}
--- javascript/new.js
+++ javascript/new.js
@@ -1,6 +1,25 @@
-  console.log(  "login succesful");
-  console.log(  "login succesful");
-  console.log(  "login succesful");
-  console.log(  "login succesful");
-
-
+let ar = [133,22,13,4] 
+// let ar = ["soni","ashu","banna"] 
+// arr.push(100,"ashu")
+ar.sort((a, b) => a - b);
+// ar.sort()
+// console.log(ar);
+// // console.log(ar.join(" and "));
+// arr.forEach((value,index,arr )=>{
+//   console.log(value,index,arr)
+// })
+let arr = {
+  a:1,b:2,c:3
+}
+for (const key in arr) {
+  if (!Object.hasOwn(arr, key)) continue;
+  {
+  const element = arr[key];
+  
+  console.log( key,element)
+  }
+}
+for (const element of ar) {
+  
+  console.log(element)
+}
