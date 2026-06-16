311ddf46f4f12ff2a460e536340377c9411a6a68
Ashu <soniashu022@gmail.com>
2026-06-16 18:40:45 +0530

    Add files via upload

---
+++ ArraysQuestion/two_Pointer/ques.java
@@ -0,0 +1,66 @@
+package ArraysQuestion.two_Pointer;
+
+import java.util.*;
+
+/**
+ * Definition for singly-linked list.
+ * public class ListNode {
+ * int val;
+ * ListNode next;
+ * ListNode() {}
+ * ListNode(int val) { this.val = val; }
+ * ListNode(int val, ListNode next) { this.val = val; this.next = next; }
+ * }
+ */
+public class ques {
+    static class ListNode {
+        int val;
+        ListNode next;
+
+        ListNode(int val) {
+            this.val = val;
+        }
+
+        ListNode(int val, ListNode next) {
+            this.val = val;
+            this.next = next;
+        }
+    }
+
+    public static void main(String[] args) {
+
+        // 1 -> 1 -> 2 -> 3 -> 3
+        ListNode head = new ListNode(1, new ListNode(1, new ListNode(2, new ListNode(3, new ListNode(3)))));
+
+        head = deleteDuplicates(head);
+
+        printList(head);
+        // System.out.println(deleteDuplicates(head));
+    }
+
+    static ListNode deleteDuplicates(ListNode head) {
+        ListNode curr = head;
+        while (curr != null && curr.next != null) {
+            if (curr.val == curr.next.val) {
+                curr.next = curr.next.next;
+            } else {
+                curr = curr.next;
+
+            }
+        }
+        return head;
+    }
+
+    static void printList(ListNode head) {
+        ListNode temp = head;
+
+        while (temp != null) {
+            System.out.print(temp.val);
+            if (temp.next != null) {
+                System.out.print(" -> ");
+            }
+            temp = temp.next;
+        }
+        System.out.println();
+    }
+}
