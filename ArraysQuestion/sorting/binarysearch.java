385bc2860a9e84eaa99c3b6c5723ef713519099f
Ashu <soniashu022@gmail.com>
2026-06-20 18:44:22 +0530

    Add files via upload

--- ArraysQuestion/sorting/binarysearch.java
+++ ArraysQuestion/sorting/binarysearch.java
@@ -1,3 +1,4 @@
+<<<<<<< HEAD
 package ArraysQuestion.sorting;
 
 public class binarysearch {
@@ -27,3 +28,34 @@ public class binarysearch {
         return -1;
     }
 }
+=======
+package ArraysQuestion.sorting;
+
+public class binarysearch {
+    public static void main(String[] args) {
+        int[] arr = { 2, 3, 4, 5, 6, 7, 8, 9, 10 };
+        int target = 6;
+        int ans = Binarysearch(arr, target);
+        System.out.println(ans);
+    }
+
+    static int Binarysearch(int[] arr, int target) {
+        int start = 0;
+        int end = arr.length - 1;
+        while (start <= end) {
+            int mid = start + (end - start) / 2;
+            if (target > arr[mid]) {
+                start = mid + 1;
+
+            } else if (target < arr[mid]) {
+                end = mid - 1;
+
+            } else {
+                return mid;
+            }
+        }
+
+        return -1;
+    }
+}
+>>>>>>> 1e38293c51807b9b4f87dc3a451d7b63fe2e7fbb
--- ArraysQuestion/sorting/ques.java
+++ ArraysQuestion/sorting/ques.java
@@ -1,3 +1,4 @@
+<<<<<<< HEAD
 package ArraysQuestion.sorting;
 
 import java.util.*;
@@ -28,3 +29,35 @@ public class ques {
     }
 
 }
+=======
+package ArraysQuestion.sorting;
+
+import java.util.*;
+
+public class ques {
+    public static void main(String[] args) {
+        String s = "anagram";
+        String t = "nagaram";
+        System.out.println(isAnagram(s,t));
+    }
+
+    static boolean isAnagram(String s, String t) {
+       if (s == null || t == null) {
+        return false;
+    }
+
+    if (s.length() != t.length()) {
+        return false;
+    }
+
+    char[] a = s.toCharArray();
+    char[] b = t.toCharArray();
+
+    Arrays.sort(a);
+    Arrays.sort(b);
+
+    return Arrays.equals(a, b);
+    }
+
+}
+>>>>>>> 1e38293c51807b9b4f87dc3a451d7b63fe2e7fbb
