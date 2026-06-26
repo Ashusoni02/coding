3e6a0ec10695b9379e6e04b66731277ff10de299
Ashu <soniashu022@gmail.com>
2026-06-26 20:52:40 +0530

    Add files via upload

---
+++ college_work/Ada/RecursiveBS.java
@@ -0,0 +1,36 @@
+package ArraysQuestion.college_work.Ada;
+
+public class RecursiveBS {
+    public static void main(String[] args) {
+    
+        int arr[] = {10, 20, 30, 40, 50, 60, 70};
+        int key = 50;
+    
+        int result = binarySearch(arr, 0, arr.length - 1, key);
+    
+        if (result == -1) {
+            System.out.println(result);
+        } else {
+            System.out.println("Element found at index " + result);
+        }
+    }
+     static int binarySearch(int arr[], int low, int high, int key) {
+
+        if (low > high) {
+            return -1;
+        }
+
+        int mid = (low + high) / 2;
+
+        if (arr[mid] == key) {
+            return mid;
+        }
+
+        if (key < arr[mid]) {
+            return binarySearch(arr, low, mid - 1, key);
+        } else {
+            return binarySearch(arr, mid + 1, high, key);
+        }
+    }
+
+}
