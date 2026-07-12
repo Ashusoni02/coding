22a9c843983992a095e49d188653365d34540ade
Ashu <soniashu022@gmail.com>
2026-07-12 19:40:03 +0530

    Add files via upload

---
+++ striver/practics/merge.java
@@ -0,0 +1,51 @@
+package practics;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+
+// import college_work.Ada.mergeSort;
+
+public class merge {
+    public static void main(String[] args) {
+        int[] arr = { 4, 5, 7, 2, 87, 3 };
+mergesort(arr, 0, arr.length - 1);
+System.out.println(Arrays.toString(arr));
+    }
+
+    public static void mergesort(int[] arr, int low, int high) {
+        if (low >= high) {
+            return;
+        }
+        int mid = (low + high) / 2;
+        mergesort(arr, low, mid);
+        mergesort(arr, mid + 1, high);
+        merge(arr, low, mid, high);
+    }
+
+    static void merge(int[] arr, int low, int mid, int high) {
+        ArrayList<Integer> list = new ArrayList<>();
+        int left = low;
+        int right = mid + 1;
+        while (left <= mid && right <= high) {
+            if (arr[left] <= arr[right]) {
+                list.add(arr[left]);
+                left++;
+            } else {
+                list.add(arr[right]);
+                right++;
+            }
+        }
+        while (left <= mid) {
+            list.add(arr[left]);
+            left++;
+        }
+        while (right <= high) {
+            list.add(arr[right]);
+            right++;
+        }
+        for (int i = low; i <= high; i++) {
+            arr[i] = list.get(i - low);
+        }
+        // return arr;
+    }
+}
