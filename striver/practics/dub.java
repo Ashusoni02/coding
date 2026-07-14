25176b79da62f5cc9a9e82f549cb09886aeb19a3
Ashu <soniashu022@gmail.com>
2026-07-14 12:24:19 +0530

    Add files via upload

--- striver/practics/dub.java
+++ striver/practics/dub.java
@@ -1,18 +1,28 @@
 package practics;
 
+import java.util.Arrays;
+
 public class dub {
     public static void main(String[] args) {
-        int[] a = {1,1,2,2,3,3};
-        int n = a.length;
-        System.out.println(checkunique(a, n));
+        int[] a = {-1,-100,3,99};
+        int k =2;
+        int n = a.length-1;
+        System.out.println(Arrays.toString(checkunique(a, n,k)));
     }
-    public static int checkunique(int[] a ,int n ){
-        int unique = 1;
-        for (int i = 0; i < n-1; i++) {
-            if (a[i]!= a[i+1]) {
-             unique++;
+    public static int[] checkunique(int[] a ,int n ,int k){
+            k= k%n;
+             reverse(a, k+1,n);
+            reverse(a, 0, k);
+            reverse(a, 0,n);
+            return a;
+    }
+       static void reverse(int[] a, int low ,int high){
+       while(low<=high) {
+        int temp = a[low];
+        a[low] =a[high];
+        a[high] = temp;
+        low++;
+        high--;}
         }
     }
-        return unique;
-    }
-}
+
